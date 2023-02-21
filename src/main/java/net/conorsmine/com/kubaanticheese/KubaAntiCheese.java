3fb83fa408ed6e8fccc795765ccf820ba14b1a99
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2023-02-21 11:20:56 +0100

    Initial commit

---
+++ src/main/java/net/conorsmine/com/kubaanticheese/KubaAntiCheese.java
@@ -0,0 +1,17 @@
+package net.conorsmine.com.kubaanticheese;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KubaAntiCheese extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
